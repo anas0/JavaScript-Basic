efad8e62d1ec24e83576d8453405e42f8fa1db79
anas0 <ashraful0a@gmail.com>
2022-11-03 23:35:03 +0600

    Variables

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
-// Say h
-//node index.js
-console.log('Hello Bismi');
+let name = "Bismi";
+console.log(name);
+
+//Cannot be reserved keyword
+//Should be meaningful
+//Cannot start with a number (1name)
+//Cannot contain a space or hyphen (-)
+//Are case-sensitive
+
+let fullName = 'jone Sina';
+let firstName = 'Jone' , lastName = 'Sina';
