7375a7ad18dece79c9db4cf1c15c9c8b7b9355e0
anas0 <ashraful0a@gmail.com>
2022-11-04 00:07:28 +0600

    Dynamic Typing

--- index.js
+++ index.js
@@ -3,3 +3,18 @@ let age = 30; //Number Literal
 let isApproved = true;  //Boolean Literal
 let firstName = undefined;
 let lastName = null;
+
+// typeof name
+// "string"
+
+// name = 10
+// 10
+
+// typeof name
+// "number" 
+
+// typeof age
+// "number"
+
+// typeof lastName
+// "object"
