bf61ac6f75d08979acff3df3b356455ef9083bea
anas0 <ashraful0a@gmail.com>
2022-11-04 00:37:12 +0600

    Arrays

--- index.js
+++ index.js
@@ -1,19 +1,5 @@
-// let name = 'Anas';
-// let age = 30;
-
-let person = {
-    name: 'Anas',
-    age: 30
-};
-
-//Dot Notation
-person.name = 'Jone';
-
-//Bracket Notation
-person['name'] = 'Bismi';
-
-//Target Property
-let selection = 'name'
-person[selection] = 'Sina';
-
-console.log(person.name);
+let selectedColor = ['red', 'green'];
+selectedColor[2] = 'blue';
+selectedColor[3] = '1';
+console.log(selectedColor);
+console.log(selectedColor.length);
