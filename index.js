68dccb65701cbc33e53b05521150c54c1f918feb
anas0 <ashraful0a@gmail.com>
2022-11-04 00:58:45 +0600

    Types of Functions

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
+//Performing a task
 function greet(name, lastName){
     console.log('Hello ' + name + ' ' + lastName);
 }
 
 greet('Jone', 'Sina');
-greet('Bismi');
+
+//Calculate a value
+function square(number){
+    return number * number;
+}
+
+let number = square(2);
+console.log(number);
